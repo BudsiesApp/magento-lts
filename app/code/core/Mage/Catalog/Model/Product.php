086cf71ef54e0ef4bfa83b8cafe9cb71d1a2f37f
Andrey Vinogradov <andrei-vinogradov@yandex.ru>
2023-02-17 11:46:04 +0300

    Refs #28409: Customer Product model and blocks - fix galleryCollection return annotation

--- app/code/core/Mage/Catalog/Model/Product.php
+++ app/code/core/Mage/Catalog/Model/Product.php
@@ -1218,7 +1218,7 @@ class Mage_Catalog_Model_Product extends Mage_Catalog_Model_Abstract
     /**
      * Retrieve media gallery images
      *
-     * @return Varien_Data_Collection
+     * @return Varien_Data_Collection|null
      */
     public function getMediaGalleryImages()
     {
